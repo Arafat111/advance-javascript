faafedcb338fcdbba77dc4eb2c54f79c0d332454
arafat111 <arafatulshezan@gmail.com>
2023-09-02 19:47:14 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// False: 
+// ""
+// 0
+// undefined
+// null
+// NaN
+
+// True
+// "0", " ", []
+let nam;
+console.log(nam);
+
+if(nam){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
